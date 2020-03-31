86a1c6734aa39ebd58cd73af31cbf38c67e9ea4f
danielsiller <danielthebaldi@gmail.com>
2020-03-31 18:24:23 -0300

    fix setting

--- src/Pages/LIOP/setting.js
+++ src/Pages/LIOP/setting.js
@@ -2,12 +2,12 @@ function setColor(name, value) {
   const limiters = [
     { name: 'geral_30d', deadline: 95, fail: 90 },
     { name: 'geral_month', deadline: 95, fail: 90 },
-    { name: 'base_30d', deadline: 98, fail: 95 },
-    { name: 'base_month', deadline: 98, fail: 95 },
-    { name: 'otif_30d', deadline: 95, fail: 90 },
-    { name: 'otif_month', deadline: 95, fail: 90 },
-    { name: 'mobile_30d', deadline: 95, fail: 90 },
-    { name: 'mobile_month', deadline: 95, fail: 90 },
+    { name: 'base_30d', deadline: 95, fail: 90 },
+    { name: 'base_month', deadline: 95, fail: 90 },
+    { name: 'otif_30d', deadline: 98, fail: 95 },
+    { name: 'otif_month', deadline: 98, fail: 95 },
+    { name: 'mobile_30d', deadline: 98, fail: 95 },
+    { name: 'mobile_month', deadline: 98, fail: 95 },
   ];
   for (const limiter of limiters) {
     if (name === limiter.name) {
